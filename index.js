a382e38f60b5f2187f5bc24c237f2b0cbc6e43c4
Anand Shanker Deb <ananddeb23@gmail.com>
2020-12-04 11:54:39 +0530

    cache

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send(`Version ${version}`);
 });
 
+app.get('/cached', (req, res) => {
+    const data = ['Tom', 'Maxine', 'David'];
+    res.header('Cache-Control', 'public, max-age=31557600')
+    res.send(data);
+  });
+
+app.get('/uncached', (req, res) => {
+    const data = ['Tom', 'Maxine', 'David'];
+    res.send(data);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
