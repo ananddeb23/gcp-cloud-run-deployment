2c113989295856e1ac7588659866da147d0616b6
Anand Shanker Deb <ananddeb23@gmail.com>
2020-12-07 10:53:54 +0530

    add redis

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const redis = require("redis");
+
+const client = redis.createClient({
+    "host": "10.223.214.131"
+  });
+  client.on("error", function(error) {
+    console.error(error);
+  });
 
 app.get('/', (req, res) => {
   const version = 5;
@@ -17,6 +25,23 @@ app.get('/uncached', (req, res) => {
     const data = ['Tom', 'Maxine', 'David'];
     res.send(data);
 });
+
+app.get('/redis', (req, res) => {
+    res.set('Cache-Control', 'no-store');
+    client.set("key", "value!", redis.print);
+    client.get("key", (err, reply) => {
+      res.send(`
+      <html>
+        <head>
+        </head>
+        <body>
+          <p>Connecting to Redis at: ${process.env.REDIS_IP}</p>
+          <p>Value of key just read: ${reply}</p>
+        </body>
+      </html>
+      `);
+      });
+    });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
